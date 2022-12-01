540025540da6f060c73815becb3419ef9bc38650
NikDoe <nikd0e@yandex.ru>
2022-12-01 10:14:16 +0300

    creating basic routing and adding styles
    - feature(index.html): create page for root route;
    - feature(404.html): create 404 page response;
    - feature(styles.css): basic styles for index & 404 pages;
    - feature(root.js): root dir of the API;

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('Hello there :)')
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 9006;
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+app.use('/', require('./routes/root'));
+
+app.all('*', (req, res) => {
+	res.status(404);
+	if (req.accepts('html')) {
+		res.sendFile(path.join(__dirname, 'views', '404.html'));
+	} else if (req.accepts('json')) {
+		res.json({ message: 'ничего не найдено' });
+	} else {
+		res.type('txt').send('ничего не найдено');
+	}
+});
+
+app.listen(PORT, () => console.log(`сервер запущен на порту ${PORT}`));
