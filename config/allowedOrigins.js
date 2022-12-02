f9fa3aa26de5685d3dfe07e3c008154c232028fc
NikDoe <nikd0e@yandex.ru>
2022-12-02 11:10:10 +0300

    add middlewares to the project
    - feature(allowedOrigins.js): add allowed sites from which you can make a request to API;
    - feature(corsOptions.js): add allowed origins to the cors options;
    - feature(server.js): add custom, built-in & 3rd party middlewares like cookie-parser;

---
+++ config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+	'http://localhost:3000',
+	// добавить сайт после деплоя
+];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const path = require('path');
+const { logger } = require('./middleware/logger');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const cookieParser = require('cookie-parser');
+const errorHandler = require('./middleware/errorHandler');
 const app = express();
 const PORT = process.env.PORT || 9006;
+
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
@@ -17,5 +27,7 @@ app.all('*', (req, res) => {
 		res.type('txt').send('ничего не найдено');
 	}
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`сервер запущен на порту ${PORT}`));
